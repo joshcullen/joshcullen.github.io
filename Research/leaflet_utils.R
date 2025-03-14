ee4ea5fca6d8fb2895e7569bbcc826bec341f715
joshcullen <joshcullen10@gmail.com>
2025-03-14 12:08:22 -0400

    Tried removing all unnecessary helper functions from leaflet_utils.R to fix GHA

--- Research/leaflet_utils.R
+++ Research/leaflet_utils.R
@@ -1,237 +1,237 @@
 
-### addImageQuery ############################################################
-##############################################################################
-#' Add image query functionality to leaflet/mapview map.
-#'
-#' @details
-#' This function enables Raster*/stars objects added to leaflet/mapview maps to
-#' be queried. Standard query is on 'mousmove', but can be changed to 'click'.
-#' Note that for this to work, the \code{layerId} needs to be the same as the
-#' one that was set in \code{\link[leaflet]{addRasterImage}} or
-#' \code{\link{addStarsImage}}. Currently only works for
-#' numeric values (i.e. numeric/integer and factor values are supported).
-#'
-#' @param map the map with the RasterLayer to be queried.
-#' @param x the RasterLayer that is to be queried.
-#' @param band for stars layers, the band number to be queried.
-#' @param group the group of the RasterLayer to be queried.
-#' @param layerId the layerId of the RasterLayer to be queried. Needs to be the
-#'   same as supplied in \code{\link[leaflet]{addRasterImage}} or
-#'   \code{\link{addStarsImage}}.
-#' @param project whether to project the RasterLayer to conform with leaflets
-#'   expected crs. Defaults to \code{TRUE} and things are likely to go haywire
-#'   if set to \code{FALSE}.
-#' @param type whether query should occur on 'mousemove' or 'click'. Defaults
-#'   to 'mousemove'.
-#' @param digits the number of digits to be shown in the display field.
-#' @param position where to place the display field. Default is 'topright'.
-#' @param prefix a character string to be shown as prefix for the layerId.
-#' @param className a character string to append to the control legend.
-#' @param ... currently not used.
-#'
-#' @return
-#' A leaflet map object.
-#'
-#' @examples
-#' if (interactive()) {
-#'   if (requireNamespace("plainview")) {
-#'     library(leaflet)
-#'     library(plainview)
-#'
-#'     leaflet() %>%
-#'       addProviderTiles("OpenStreetMap") %>%
-#'       addRasterImage(poppendorf[[1]], project = TRUE, group = "poppendorf",
-#'                      layerId = "poppendorf") %>%
-#'       addImageQuery(poppendorf[[1]], project = TRUE,
-#'                     layerId = "poppendorf") %>%
-#'       addLayersControl(overlayGroups = "poppendorf")
+#' ### addImageQuery ############################################################
+#' ##############################################################################
+#' #' Add image query functionality to leaflet/mapview map.
+#' #'
+#' #' @details
+#' #' This function enables Raster*/stars objects added to leaflet/mapview maps to
+#' #' be queried. Standard query is on 'mousmove', but can be changed to 'click'.
+#' #' Note that for this to work, the \code{layerId} needs to be the same as the
+#' #' one that was set in \code{\link[leaflet]{addRasterImage}} or
+#' #' \code{\link{addStarsImage}}. Currently only works for
+#' #' numeric values (i.e. numeric/integer and factor values are supported).
+#' #'
+#' #' @param map the map with the RasterLayer to be queried.
+#' #' @param x the RasterLayer that is to be queried.
+#' #' @param band for stars layers, the band number to be queried.
+#' #' @param group the group of the RasterLayer to be queried.
+#' #' @param layerId the layerId of the RasterLayer to be queried. Needs to be the
+#' #'   same as supplied in \code{\link[leaflet]{addRasterImage}} or
+#' #'   \code{\link{addStarsImage}}.
+#' #' @param project whether to project the RasterLayer to conform with leaflets
+#' #'   expected crs. Defaults to \code{TRUE} and things are likely to go haywire
+#' #'   if set to \code{FALSE}.
+#' #' @param type whether query should occur on 'mousemove' or 'click'. Defaults
+#' #'   to 'mousemove'.
+#' #' @param digits the number of digits to be shown in the display field.
+#' #' @param position where to place the display field. Default is 'topright'.
+#' #' @param prefix a character string to be shown as prefix for the layerId.
+#' #' @param className a character string to append to the control legend.
+#' #' @param ... currently not used.
+#' #'
+#' #' @return
+#' #' A leaflet map object.
+#' #'
+#' #' @examples
+#' #' if (interactive()) {
+#' #'   if (requireNamespace("plainview")) {
+#' #'     library(leaflet)
+#' #'     library(plainview)
+#' #'
+#' #'     leaflet() %>%
+#' #'       addProviderTiles("OpenStreetMap") %>%
+#' #'       addRasterImage(poppendorf[[1]], project = TRUE, group = "poppendorf",
+#' #'                      layerId = "poppendorf") %>%
+#' #'       addImageQuery(poppendorf[[1]], project = TRUE,
+#' #'                     layerId = "poppendorf") %>%
+#' #'       addLayersControl(overlayGroups = "poppendorf")
+#' #'   }
+#' #' }
+#' #'
+#' #' @importFrom raster projectExtent projectRaster as.matrix
+#' #'
+#' #' @export addImageQuery
+#' #' @name addImageQuery
+#' #' @rdname addImageQuery
+#' addImageQuery = function(map,
+#'                          x,
+#'                          band = 1,
+#'                          group = NULL,
+#'                          layerId = NULL,
+#'                          project = TRUE,
+#'                          type = c("mousemove", "click"),
+#'                          digits,
+#'                          position = 'topright',
+#'                          prefix = 'Layer',
+#'                          className = "",
+#'                          ...) {
+#'   
+#'   if (inherits(map, "mapview")) map = mapview2leaflet(map)
+#'   
+#'   type = match.arg(type)
+#'   if (missing(digits)) digits = "null"
+#'   if (is.null(group)) group = "stars"
+#'   if (is.null(layerId)) layerId = group
+#'   
+#'   jsgroup <- gsub(".", "", make.names(group), fixed = TRUE)
+#'   
+#'   tmp <- leafem:::makepathStars(as.character(jsgroup))
+#'   pathDatFn <- tmp[[2]][1]
+#'   # starspathDatFn <- tmp[[3]][1]
+#'   # datFn <- tmp[[4]][1]
+#'   
+#'   if (project) {
+#'     if (inherits(x, "stars")) {
+#'       if (utils::packageVersion("stars") >= "0.4-1") {
+#'         projected = stars::st_warp(x, crs = 4326)
+#'       } else {
+#'         projected <- sf::st_transform(x, crs = 4326)
+#'       }
+#'     }
+#'     if (inherits(x, "SpatRaster")) {
+#'       projected <- terra::project(x, y = "EPSG:4326")
+#'       }
+#'     if (inherits(x, "Raster")) {
+#'       projected = raster::projectRaster(
+#'         x
+#'         , raster::projectExtent(x, crs = sf::st_crs(4326)$proj4string)
+#'         , method = "ngb"
+#'       )
+#'     }
+#'   } else {
+#'     projected <- x
 #'   }
+#'   
+#'   pre <- paste0('var data = data || {}; data["', layerId, '"] = ')
+#'   writeLines(pre, pathDatFn)
+#'   cat('[', image2Array(projected, band = band), '];',
+#'       file = pathDatFn, sep = "", append = TRUE)
+#'   
+#'   ## check for existing layerpicker control
+#'   ctrlid = leafem:::getCallEntryFromMap(map, "addControl")
+#'   ctrl_nm = paste("imageValues", layerId, sep = "-")
+#'   imctrl = unlist(sapply(ctrlid, function(i) {
+#'     ctrl_nm %in% map$x$calls[[i]]$args
+#'   }))
+#'   ctrlid = ctrlid[imctrl]
+#'   
+#'   # map = leaflet::clearControls(map)
+#'   
+#'   if (length(ctrlid) == 0) {
+#'     # must add empty character instead of NULL for html with addControl
+#'     map = leaflet::addControl(
+#'       map,
+#'       html = "",
+#'       layerId = ctrl_nm,
+#'       position = position,
+#'       className = paste("info legend", className)
+#'     )
+#'   }
+#'   
+#'   sm <- leafem:::createFileId() #sample(1:1000, 1)
+#'   map$dependencies <- c(map$dependencies,
+#'                         leafem:::starsDataDependency(jFn = pathDatFn,
+#'                                             counter = 1,
+#'                                             group = paste0(layerId,"_",sm)))
+#'   map$dependencies = c(map$dependencies,
+#'                        list(htmltools::htmlDependency(
+#'                          version = "0.0.1",
+#'                          name = "joda",
+#'                          src = system.file("htmlwidgets/lib/joda",
+#'                                            package = "leafem"),
+#'                          script = c("joda.js",
+#'                                     "addImageQuery-bindings.js"))
+#'                        ))
+#'   
+#'   bounds <- as.numeric(sf::st_bbox(projected))
+#'   
+#'   leaflet::invokeMethod(
+#'     map
+#'     , NULL
+#'     , "addImageQuery"
+#'     , layerId
+#'     , bounds
+#'     , type
+#'     , digits
+#'     , prefix
+#'   )
 #' }
-#'
-#' @importFrom raster projectExtent projectRaster as.matrix
-#'
-#' @export addImageQuery
-#' @name addImageQuery
-#' @rdname addImageQuery
-addImageQuery = function(map,
-                         x,
-                         band = 1,
-                         group = NULL,
-                         layerId = NULL,
-                         project = TRUE,
-                         type = c("mousemove", "click"),
-                         digits,
-                         position = 'topright',
-                         prefix = 'Layer',
-                         className = "",
-                         ...) {
-  
-  if (inherits(map, "mapview")) map = mapview2leaflet(map)
-  
-  type = match.arg(type)
-  if (missing(digits)) digits = "null"
-  if (is.null(group)) group = "stars"
-  if (is.null(layerId)) layerId = group
-  
-  jsgroup <- gsub(".", "", make.names(group), fixed = TRUE)
-  
-  tmp <- leafem:::makepathStars(as.character(jsgroup))
-  pathDatFn <- tmp[[2]][1]
-  # starspathDatFn <- tmp[[3]][1]
-  # datFn <- tmp[[4]][1]
-  
-  if (project) {
-    if (inherits(x, "stars")) {
-      if (utils::packageVersion("stars") >= "0.4-1") {
-        projected = stars::st_warp(x, crs = 4326)
-      } else {
-        projected <- sf::st_transform(x, crs = 4326)
-      }
-    }
-    if (inherits(x, "SpatRaster")) {
-      projected <- terra::project(x, y = "EPSG:4326")
-      }
-    if (inherits(x, "Raster")) {
-      projected = raster::projectRaster(
-        x
-        , raster::projectExtent(x, crs = sf::st_crs(4326)$proj4string)
-        , method = "ngb"
-      )
-    }
-  } else {
-    projected <- x
-  }
-  
-  pre <- paste0('var data = data || {}; data["', layerId, '"] = ')
-  writeLines(pre, pathDatFn)
-  cat('[', image2Array(projected, band = band), '];',
-      file = pathDatFn, sep = "", append = TRUE)
-  
-  ## check for existing layerpicker control
-  ctrlid = leafem:::getCallEntryFromMap(map, "addControl")
-  ctrl_nm = paste("imageValues", layerId, sep = "-")
-  imctrl = unlist(sapply(ctrlid, function(i) {
-    ctrl_nm %in% map$x$calls[[i]]$args
-  }))
-  ctrlid = ctrlid[imctrl]
-  
-  # map = leaflet::clearControls(map)
-  
-  if (length(ctrlid) == 0) {
-    # must add empty character instead of NULL for html with addControl
-    map = leaflet::addControl(
-      map,
-      html = "",
-      layerId = ctrl_nm,
-      position = position,
-      className = paste("info legend", className)
-    )
-  }
-  
-  sm <- leafem:::createFileId() #sample(1:1000, 1)
-  map$dependencies <- c(map$dependencies,
-                        leafem:::starsDataDependency(jFn = pathDatFn,
-                                            counter = 1,
-                                            group = paste0(layerId,"_",sm)))
-  map$dependencies = c(map$dependencies,
-                       list(htmltools::htmlDependency(
-                         version = "0.0.1",
-                         name = "joda",
-                         src = system.file("htmlwidgets/lib/joda",
-                                           package = "leafem"),
-                         script = c("joda.js",
-                                    "addImageQuery-bindings.js"))
-                       ))
-  
-  bounds <- as.numeric(sf::st_bbox(projected))
-  
-  leaflet::invokeMethod(
-    map
-    , NULL
-    , "addImageQuery"
-    , layerId
-    , bounds
-    , type
-    , digits
-    , prefix
-  )
-}
-
-###################################
-
-
-stars2Array = function(x, band = 1) {
-  if(length(dim(x)) == 2) layer = x[[1]] else layer = x[[1]][, , band]
-  paste(
-    sapply(seq(nrow(x[[1]])), function(i) {
-      paste0(
-        '['
-        , gsub(
-          "NA"
-          , "null"
-          , paste(as.numeric(layer[i, ]), collapse = ",")
-        )
-        , ']'
-      )
-    }),
-    collapse = ","
-  )
-}
+#' 
+#' ###################################
+#' 
+#' 
+#' stars2Array = function(x, band = 1) {
+#'   if(length(dim(x)) == 2) layer = x[[1]] else layer = x[[1]][, , band]
+#'   paste(
+#'     sapply(seq(nrow(x[[1]])), function(i) {
+#'       paste0(
+#'         '['
+#'         , gsub(
+#'           "NA"
+#'           , "null"
+#'           , paste(as.numeric(layer[i, ]), collapse = ",")
+#'         )
+#'         , ']'
+#'       )
+#'     }),
+#'     collapse = ","
+#'   )
+#' }
 
 
 ###################################
 
 
-SpatRaster2Array = function(x) {
-  x = terra::as.matrix(x, wide = TRUE)
-  paste(
-    sapply(seq(ncol(x)), function(i) {
-      paste0(
-        '['
-        , gsub(
-          "NA"
-          , "null"
-          , paste(as.matrix(x)[, i], collapse = ",")
-        )
-        , ']'
-      )
-    }),
-    collapse = ","
-  )
-}
+# SpatRaster2Array = function(x) {
+#   x = terra::as.matrix(x, wide = TRUE)
+#   paste(
+#     sapply(seq(ncol(x)), function(i) {
+#       paste0(
+#         '['
+#         , gsub(
+#           "NA"
+#           , "null"
+#           , paste(as.matrix(x)[, i], collapse = ",")
+#         )
+#         , ']'
+#       )
+#     }),
+#     collapse = ","
+#   )
+# }
 
 
 ###################################
 
 
-rasterLayer2Array = function(x) {
-  x = as.matrix(x)
-  paste(
-    sapply(seq(ncol(x)), function(i) {
-      paste0(
-        '['
-        , gsub(
-          "NA"
-          , "null"
-          , paste(as.matrix(x)[, i], collapse = ",")
-        )
-        , ']'
-      )
-    }),
-    collapse = ","
-  )
-}
+# rasterLayer2Array = function(x) {
+#   x = as.matrix(x)
+#   paste(
+#     sapply(seq(ncol(x)), function(i) {
+#       paste0(
+#         '['
+#         , gsub(
+#           "NA"
+#           , "null"
+#           , paste(as.matrix(x)[, i], collapse = ",")
+#         )
+#         , ']'
+#       )
+#     }),
+#     collapse = ","
+#   )
+# }
 
 
 ###################################
 
 
-image2Array = function(x, band = 1) {
-  switch(class(x)[1],
-         "SpatRaster" = SpatRaster2Array(x),
-         "stars" = stars2Array(x, band = band),
-         "RasterLayer" = rasterLayer2Array(x))
-}
+# image2Array = function(x, band = 1) {
+#   switch(class(x)[1],
+#          "SpatRaster" = SpatRaster2Array(x),
+#          "stars" = stars2Array(x, band = band),
+#          "RasterLayer" = rasterLayer2Array(x))
+# }
 
 
 ###################################
@@ -355,47 +355,47 @@ addLegend_decreasing <- function (map,
 
 
 # Function to change CRS of coords, but leave as data.frame instead of converting to {sf} obj
-st_changeCoords = function(data, coords, crs_in, crs_out) {
-  # returns data.frame w/ cols named 'x' and 'y' for transformed coords
-  
-  dat.sf <- sf::st_as_sf(data, coords = coords, crs = crs_in) %>% 
-    sf::st_transform(dat.sf, crs = crs_out) %>% 
-    mutate(x = st_coordinates(.)[,1],
-           y = st_coordinates(.)[,2]) %>% 
-    st_drop_geometry()
-  
-  return(dat.sf)
-}
+# st_changeCoords = function(data, coords, crs_in, crs_out) {
+#   # returns data.frame w/ cols named 'x' and 'y' for transformed coords
+#   
+#   dat.sf <- sf::st_as_sf(data, coords = coords, crs = crs_in) %>% 
+#     sf::st_transform(dat.sf, crs = crs_out) %>% 
+#     mutate(x = st_coordinates(.)[,1],
+#            y = st_coordinates(.)[,2]) %>% 
+#     st_drop_geometry()
+#   
+#   return(dat.sf)
+# }
 
 
 ###################################
 
 
 # function to convert list object from rerddapXtracto::rxtracto() into a {terra} SpatRaster object
-array2rast <- function(lon, lat, var, time, extent) {
-  #lon: a vector of longitude values
-  #lat: a vector of latitude values
-  #var: an array of values for the variable of interest where the 1st two dimensions denote the spatial grid and the third dimensions represents the number of datetimes
-  #time: a vector of dates associated with each 2D array from `var`
-  #extent: a SpatRaster extent on which to spatially define the raster; ordered as xmin, xmax, ymin, ymax
-  
-  dims <- dim(var)
-  
-  # check dims in case "altitude/depth" is included; this code will only select 1st slice of altitude
-  if (length(dims) == 4) {
-    var <- var[,,1,]
-  } else {
-    var <- var
-  }
-  
-  dims1 <- dim(var)
-  
-  rast1 <- terra::rast(t(var), crs = 'EPSG:4326', extent = extent) %>%
-      terra::flip(direction = "vertical")
-  names(rast1) <- time
-  
-  return(rast1)
-}
+# array2rast <- function(lon, lat, var, time, extent) {
+#   #lon: a vector of longitude values
+#   #lat: a vector of latitude values
+#   #var: an array of values for the variable of interest where the 1st two dimensions denote the spatial grid and the third dimensions represents the number of datetimes
+#   #time: a vector of dates associated with each 2D array from `var`
+#   #extent: a SpatRaster extent on which to spatially define the raster; ordered as xmin, xmax, ymin, ymax
+#   
+#   dims <- dim(var)
+#   
+#   # check dims in case "altitude/depth" is included; this code will only select 1st slice of altitude
+#   if (length(dims) == 4) {
+#     var <- var[,,1,]
+#   } else {
+#     var <- var
+#   }
+#   
+#   dims1 <- dim(var)
+#   
+#   rast1 <- terra::rast(t(var), crs = 'EPSG:4326', extent = extent) %>%
+#       terra::flip(direction = "vertical")
+#   names(rast1) <- time
+#   
+#   return(rast1)
+# }
 
 
 ###################################
